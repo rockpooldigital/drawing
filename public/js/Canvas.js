3c1b4832038d364055133cd396910df3f4028fff
Tom Haigh <tom.haigh@rockpooldigital.com>
2012-12-07 16:18:53 +0000

    day2

---
+++ public/js/Canvas.js
@@ -0,0 +1,101 @@
+if (typeof(window.RockDrawing) === "undefined")	window.RockDrawing = {};
+
+window.RockDrawing.Canvas = function(canvas) {
+	var  _backCanvas = canvas;
+	var _context = canvas.getContext('2d');
+	
+	function drawSmoothPath(set) {
+		_context.moveTo(set[0][0], set[0][1]);
+
+		if (set.length > 4) {				 
+			for (var i = 1; i < set.length - 2; i++) {
+		    var c = (set[i][0] + set[i + 1][0]) / 2;
+		    var d = (set[i][1] + set[i + 1][1]) / 2;
+		 
+		    _context.quadraticCurveTo(set[i][0], set[i][1], c, d);
+			}
+		 
+			// For the last 2 points
+			_context.quadraticCurveTo(
+			    set[i][0],
+			    set[i][1],
+			    set[i + 1][0],
+			    set[i + 1][1]
+			);
+		} else {
+			for (var i = 1; i < set.length ; i++) {
+		    _context.lineTo(set[i][0], set[i][1]);
+			}
+		}
+	}
+
+	function redrawCanvas(canvas) {
+		var buffer = document.createElement('canvas');
+		var bounds = canvas.getBoundingClientRect();
+		buffer.width =  bounds.width;
+		buffer.height = bounds.height;
+
+		var bufferCtx = buffer.getContext('2d');
+		var context = canvas.getContext('2d');
+
+		bufferCtx.drawImage(canvas, 0, 0);
+
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		canvas.width = bounds.width;
+		canvas.height = bounds.height;
+
+		context.drawImage(buffer, 0, 0);
+	}
+
+	function drawSmoothLine(set, width, colour) {
+		_context.beginPath();
+		_context.lineWidth = width ;
+		_context.strokeStyle = colour;
+		_context.lineCap = 'round';
+		drawSmoothPath(set);
+		_context.stroke();
+	}
+
+	function drawLine(x1,y1,x2,y2,width, colour) {
+		_context.beginPath();
+		_context.lineWidth = width;
+		_context.strokeStyle = colour;
+		_context.moveTo(x1, y1);
+		_context.lineTo(x2, y2);
+		_context.stroke();
+	}
+
+	function placePoint(x,y, width, colour) {
+		_context.beginPath();
+		var r = Math.floor((width + 0.0)/ 2);
+		_context.arc(x, y, r , 0, 2 * Math.PI, false);
+		_context.fillStyle = colour;
+		_context.fill();
+	}
+
+	function drawPoints(set, w, c) {
+	if (set.length === 1) {
+			placePoint(set[0][0], set[0][1], w, c);
+		} else {
+			drawSmoothLine(set, w,c );
+		}
+	}
+
+	return {
+		pushCommand : function(command) {
+			drawPoints(
+				command.path,
+				command.toolSize,
+				command.colour
+			);
+		},
+		//initPen : initPen,
+		placePoint :placePoint,
+		drawSmoothLine : drawSmoothLine,
+		clear :function() {
+			_context.clearRect(0, 0, canvas.width, canvas.height);
+		},
+		drawLine: drawLine
+	}
+}
---
+++ public/js/drawing/Surface.js
@@ -0,0 +1,121 @@
+
+window.RockDrawing.TOOL_PEN = 1;
+window.RockDrawing.CreateSurface = function(container) {
+	var BUFFER_SIZE=  15;
+
+	var surface = {
+		TOOL_PEN : 1,
+		onDataReady : null
+	};
+
+	var _x, _y, 
+			_colour, _isDrawing = false, 
+			ctx, 
+			_canvas, _tool = surface.TOOL_PEN, 
+	 		_toolSize = 4,
+	 		 _points = [] , _pointIndex = 0, _colour = "#000";
+
+
+
+	var bounds = container.getBoundingClientRect();
+	console.log(bounds);
+
+		//canvas
+	_backCanvas = document.createElement("canvas");
+	_backCanvas.classList.add("back");
+	container.appendChild(_backCanvas);
+	_backCanvas.width  = bounds.width;
+	_backCanvas.height = bounds.height;
+	//front drawing canvas
+	_canvas = document.createElement("canvas");
+	_canvas.classList.add("front");
+	container.appendChild(_canvas);
+		_canvas.width  = bounds.width;
+	_canvas.height = bounds.height;
+	
+
+	var _backing = RockDrawing.Canvas(_backCanvas);
+	var _front = RockDrawing.Canvas(_canvas);
+	var ctx = _canvas.getContext('2d'); 
+
+	function pushPoints(start) {
+		//console.log("pushing", start);
+		var set = points.slice(start);
+		console.log(points.length);
+		var command = {
+			tool : _tool,
+			toolSize : _toolSize,
+			path : set,
+			colour : _colour				
+		};
+
+		/*if (surface.onDataReady) {
+			surface.onDataReady(command);
+		}*/
+		console.log(command);
+		_backing.pushCommand(command);		
+		_front.clear();
+	}
+
+	function beginDraw(x,y)  { 
+		if (_isDrawing) return;
+		_isDrawing = true;
+		
+		_front.placePoint(x, y, _toolSize, _colour);
+		_x=x;_y=y;
+		_pointIndex = 0;
+		points = [[x,y]];
+		
+		ctx.beginPath();
+		ctx.lineWidth = _toolSize;
+		ctx.strokeStyle = _colour;
+		ctx.lineCap = 'round';
+	}
+
+	function draw(x,y) {
+		if (!_isDrawing) return;
+		if (x=== _x && y===_y) return;
+	
+		_front.drawLine(_x, _y, x, y);
+
+		_x=x;_y=y;
+
+		points.push([x,y]);
+		++_pointIndex;
+
+		if (((_pointIndex+1)  % BUFFER_SIZE) === 0 ) {
+			var start = _pointIndex - BUFFER_SIZE;
+			if (start < 0) start = 0;
+			pushPoints(start);
+		}
+	}
+
+	function endDraw() {// alert('blur');
+		if (!_isDrawing) return;
+		_isDrawing = false;
+	
+		//push remaining points
+		var start = BUFFER_SIZE * Math.floor((_pointIndex + 1) / BUFFER_SIZE);
+		if (start > 0) --start;
+		pushPoints(start);
+	} 
+
+	var yOffset = _canvas.offsetTop;
+
+	surface.penDown = beginDraw;
+	surface.penMove = draw;
+	surface.penUp = endDraw;
+	surface.setTool = function(tool, size) {
+		_toolSize = size;
+		_tool = tool;
+	}
+	surface.setColour = function(col) {
+		_colour = col;
+	}
+	surface.frontCanvas = _canvas;
+	/*surface.adjustSizeAndRedraw = function(){ 
+		canvasHelper.redrawCanvas(_backCanvas);
+		canvasHelper.redrawCanvas(_canvas);
+	}*/
+	return surface;
+}
